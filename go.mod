927474f8168542acae9f7812f50607eba26e7904
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-06-16 18:14:51 +0200

    Added Attribute Type

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/baldurstod/go-dmx
+
+go 1.19
---
+++ zzzz_test.go
@@ -0,0 +1,27 @@
+package dmx_test
+
+import (
+	"github.com/baldurstod/go-dmx"
+	"log"
+	"testing"
+)
+
+func TestAttributesTypes(t *testing.T) {
+	log.Println(dmx.AT_FIRST_VALUE_TYPE, dmx.AT_VMATRIX, dmx.AT_FIRST_ARRAY_TYPE, dmx.AT_TYPE_COUNT)
+
+	if dmx.AT_FIRST_VALUE_TYPE != 1 {
+		t.Error("dmx.AT_FIRST_VALUE_TYPE != 1")
+	}
+
+	if dmx.AT_VMATRIX != 15 {
+		t.Error("dmx.AT_VMATRIX != 15")
+	}
+
+	if dmx.AT_FIRST_ARRAY_TYPE != 16 {
+		t.Error("dmx.AT_FIRST_ARRAY_TYPE != 16")
+	}
+
+	if dmx.AT_TYPE_COUNT != 31 {
+		t.Error("dmx.AT_TYPE_COUNT != 31")
+	}
+}
