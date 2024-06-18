7337700778c9aa635d263b398395790e81f6a0c8
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-06-18 14:45:00 +0200

    Added CreateObjectId()

--- dmobjectid.go
+++ dmobjectid.go
@@ -1,3 +1,21 @@
 package dmx
 
-type DmObjectId [16]byte
+import (
+	"crypto/rand"
+)
+
+type DmObjectId = [16]byte
+
+func CreateObjectId() DmObjectId {
+	var b [16]byte
+
+	_, err := rand.Read(b[:])
+	if err != nil {
+		panic("Can't create element id")
+	}
+
+	b[8] = (b[8] | 0x80) & 0xBF
+	b[6] = (b[6] | 0x40) & 0x4F
+
+	return b
+}
