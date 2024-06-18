8d971ddf609e234fc10dc43d50aa82565c6c7b91
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-06-18 18:00:16 +0200

    Serialize more types

--- dmattribute.go
+++ dmattribute.go
@@ -1,5 +1,10 @@
 package dmx
 
+import (
+	"strconv"
+	"fmt"
+)
+
 type DmAttribute struct {
 	name          string
 	attributeType DmAttributeType
@@ -103,4 +108,71 @@ func (attribute *DmAttribute) SetValues(value ...interface{}) {
 
 func (attribute *DmAttribute) GetOwner() *DmElement {
 	return attribute.owner
+}
+
+func (attribute *DmAttribute) StringValue() string {
+	switch attribute.attributeType {
+	case AT_INT:
+		return strconv.FormatInt(attribute.value.(int64), 10)
+	case AT_FLOAT:
+		return strconv.FormatFloat(attribute.value.(float64), 'g', -1, 64)
+	case AT_BOOL:
+		return strconv.FormatBool(attribute.value.(bool))
+	case AT_STRING:
+		return attribute.value.(string)
+	case AT_OBJECTID:
+		attribute.value = new(DmObjectId)
+	case AT_COLOR:
+		fallthrough
+	case AT_VECTOR4:
+		fallthrough
+	case AT_QUATERNION:
+		v := attribute.value.([4]float64)
+		c := fmt.Sprintf("%g %g %g %g", v[0], v[1], v[2], v[3])
+		return c
+	case AT_VECTOR2:
+		v := attribute.value.([2]float64)
+		c := fmt.Sprintf("%g %g", v[0], v[1])
+		return c
+	case AT_VECTOR3:
+		fallthrough
+	case AT_QANGLE:
+		v := attribute.value.([3]float64)
+		c := fmt.Sprintf("%g %g %g", v[0], v[1], v[2])
+		return c
+	case AT_VMATRIX:
+		v := attribute.value.([16]float64)
+		c := fmt.Sprintf("%g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g", v[0], v[1], v[2], v[3], v[4], v[5], v[6], v[7], v[8], v[9], v[10], v[11], v[12], v[13], v[14], v[15])
+		return c
+
+	case AT_ELEMENT_ARRAY:
+		attribute.value = make([]*DmElement, 0)
+	case AT_INT_ARRAY:
+		attribute.value = make([]int64, 0)
+	case AT_FLOAT_ARRAY:
+		attribute.value = make([]float64, 0)
+	case AT_BOOL_ARRAY:
+		attribute.value = make([]bool, 0)
+	case AT_STRING_ARRAY:
+		attribute.value = make([]string, 0)
+	case AT_OBJECTID_ARRAY:
+		attribute.value = make([]*DmObjectId, 0)
+	case AT_COLOR_ARRAY:
+		attribute.value = make([][]float64, 0)
+	case AT_VECTOR2_ARRAY:
+		attribute.value = make([][]float64, 0)
+	case AT_VECTOR3_ARRAY:
+		attribute.value = make([][]float64, 0)
+	case AT_VECTOR4_ARRAY:
+		attribute.value = make([][]float64, 0)
+	case AT_QANGLE_ARRAY:
+		attribute.value = make([][]float64, 0)
+	case AT_QUATERNION_ARRAY:
+		attribute.value = make([][]float64, 0)
+	case AT_VMATRIX_ARRAY:
+		attribute.value = make([][]float64, 0)
+	default:
+		panic("Unknown attribute type in StringValue " + type_to_string[attribute.attributeType])
+	}
+	return ""
 }
